f9f4df659d47220e80efc86f710dec195425a1bf
Kjell Heinsen <ich@kjell-heinsen.de>
2024-12-12 21:04:47 +0100

    Add Credits Sample

---
+++ sample_sql_credits.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_TYPEDASH','mysql');
+define('DB_HOSTDASH','localhost');
+define('DB_NAMEDASH','######');
+define('DB_USERDASH','######');
+define('DB_PASSDASH','######');
+
+
+
+
+
+
